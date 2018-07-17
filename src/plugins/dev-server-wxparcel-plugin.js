1c640707903390e5e4fb7446ac8c95044e67803f
David Jones <qowera@qq.com>
2018-07-17 15:25:51 +0800

    优化代码

--- src/plugins/dev-server-wxparcel-plugin.js
+++ src/plugins/dev-server-wxparcel-plugin.js
@@ -10,19 +10,46 @@ export default class DevServerPlugin {
     this.options = Object.assign({ port: 3000 }, options)
   }
 
+  enableCors (response) {
+    response.setHeader('Access-Control-Allow-Origin', '*')
+    response.setHeader(
+      'Access-Control-Allow-Methods',
+      'GET, HEAD, PUT, PATCH, POST, DELETE'
+    )
+
+    response.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Content-Type'
+    )
+  }
+
+  setHeaders (response) {
+    this.enableCors(response)
+  }
+
   async applyAsync (options, printer) {
     options = defaultsDeep(options, this.options)
     let { staticDir, pubPath, port } = options
 
-    let serve = ServeStatic(staticDir)
+    let serve = ServeStatic(staticDir, {
+      index: false,
+      setHeaders: this.setHeaders.bind(this)
+    })
+
     let server = http.createServer((request, response) => {
       serve(request, response, Finalhandler(request, response))
     })
 
     server.listen(port, '0.0.0.0')
 
-    printer.layze(`Static server is running at ${colors.cyan.bold(`${ip.address()}:${port}`)}`)
-    printer.layze(`Static output is served from ${colors.cyan.bold(pubPath)}`)
-    printer.layze('')
+    server.on('error', (error) => {
+      printer.error(error)
+    })
+
+    server.once('listening', () => {
+      printer.layze(`Static server is running at ${colors.cyan.bold(`${ip.address()}:${port}`)}`)
+      printer.layze(`Static output is served from ${colors.cyan.bold(pubPath)}`)
+      printer.layze('')
+    })
   }
 }
