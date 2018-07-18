146199c0ca0ee6c25d5ad7588a7c82ffd88b162c
David Jones <qowera@qq.com>
2018-07-18 10:24:10 +0800

    添加 CI 配置信息

---
+++ test/parcel.spec.js
@@ -0,0 +1,13 @@
+/* eslint max-nested-callbacks: off */
+/* eslint no-unused-expressions: off */
+/* eslint-env mocha */
+
+import 'lodash'
+
+describe('Parcel 模块', function () {
+  describe('测试编译流程', function () {
+    it('能寻找所有入口', function () {
+
+    })
+  })
+})
