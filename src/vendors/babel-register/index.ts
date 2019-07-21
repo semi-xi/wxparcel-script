665ca9319625ac5117e8748b34fa8617a3b1bb5f
DavidJones <qowera@qq.com>
2019-07-21 12:45:24 +0800

    删除不必要文件

--- src/vendors/babel-register/index.ts
+++ src/vendors/babel-register/index.ts
@@ -3,7 +3,7 @@ import * as path from 'path'
 import GlobalOptionManager from '../../services/option-manager'
 import { localRequire } from '../../share/module'
 
-export default async function (babelFile: string) {
+export default async function babelRequire (babelFile: string) {
   let babelrc = path.join(GlobalOptionManager.rootDir, './.babelrc')
   if (fs.existsSync(babelrc)) {
     let babelConfig = fs.readJSONSync(babelrc)
